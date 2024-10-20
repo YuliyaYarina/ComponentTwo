5d15cfed11a348e4574a9b44d887800a763c6e09
Yuliya <Yulechka140200@gmail.com>
2024-10-20 19:04:01 +0500

    приложение собрано

---
+++ src/main/java/org/example/virtualtemperaturecontroller/VirtualTemperatureControllerApplication.java
@@ -0,0 +1,13 @@
+package org.example.virtualtemperaturecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTemperatureControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualTemperatureControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/virtualtemperaturecontroller/VirtualTemperatureControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.virtualtemperaturecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualTemperatureControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
